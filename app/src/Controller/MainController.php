8b7b9dd7cb08221185cf7ed37b9f75f40dbbc86a
Luwangel <adrien.amoros@gmail.com>
2017-11-06 16:21:12 +0100

    Display a shuffled puzzle

--- app/src/Controller/MainController.php
+++ app/src/Controller/MainController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class MainController
-{
-    public function start()
-    {
-        return new Response(
-            '<html><body></body></html>'
-        );
-    }
+class MainController extends Controller {
+  private $twig;
+
+  public function __construct(\Twig_Environment $twig) {
+    $this->twig = $twig;
+  }
+ 
+  public function start() {
+    return new Response($this->twig->render('main.html.twig'));
+  }
 }
---
+++ app/tests/Controller/MainControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase {
+    public function testStart() {
+
+      $client = static::createClient();
+
+      $crawler = $client->request('GET', '/');
+
+      $this->assertGreaterThan(
+          0,
+          $crawler->filter('html:contains("Welcome to the 15 puzzle game!")')->count()
+      );
+    }
+}
