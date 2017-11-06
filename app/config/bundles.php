8b1a4af3056d3019f7a69db5753778677cd7f3b8
Luwangel <adrien.amoros@gmail.com>
2017-11-06 14:17:33 +0100

    Use csa guzzle bundle instead of guzzle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Csa\Bundle\GuzzleBundle\CsaGuzzleBundle::class => ['all' => true],
 ];
