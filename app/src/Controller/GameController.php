639e2367b3457290a034bd86e1d4db5b02a7f96d
Luwangel <adrien.amoros@gmail.com>
2017-11-09 17:09:47 +0100

    Set the game token as cookie

--- app/src/Controller/GameController.php
+++ app/src/Controller/GameController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use App\Api\GameApi;
 use App\Entity\Game;
@@ -12,46 +13,69 @@ use App\Repository\GameRepository;
 
 class GameController extends Controller {
   public const DEFAULT_SIZE = 4;
+  public const COOKIE_NAME = 'current-puzzle';
+
   private $api;
   private $twig;
 
   public function __construct(\Twig_Environment $twig, GameApi $gameApi, GameRepository $gameRepository) {
-    $this->api = $gameApi;
     $this->twig = $twig;
+    $this->api = $gameApi;
     $this->gameRepository = $gameRepository;
   }
 
-  private function renderGrid(string $id, Game $game) {
+  private function hasAccess($request, $game) {
+    return $request->cookies->get(self::COOKIE_NAME) == $game->getToken();
+  }
+
+  private function setAccess($response, $game) {
+    $response->headers->setCookie(new Cookie(self::COOKIE_NAME, $game->getToken()));
+  }
+
+  private function removeAccess($response) {
+    $response->headers->clearCookie(self::COOKIE_NAME);  
+  }
+
+  private function renderGrid(string $id, Game $game, $hasAccess) {
     return new Response($this->twig->render('game.html.twig', [
       'id' => $id,
       'grid' => $game->getCurrentGrid(),
       'turn' => $game->getTurn(),
-      'isVictory' => $game->getIsVictory()
+      'isVictory' => $game->getIsVictory(),
+      'hasAccess' => $hasAccess
     ]));
   }
 
-  public function play(string $id) {
+  public function play(Request $request, string $id) {
     $game = $this->gameRepository->findGameById($id);
-    return $this->renderGrid($id, $game);
+    return $this->renderGrid($id, $game, $this->hasAccess($request, $game));
   }
 
   public function new() {
     $game = $this->api->new(self::DEFAULT_SIZE);
     $this->gameRepository->save($game);
-    return $this->redirectToRoute('game', array('id' => $game->getId()));
+
+    $response = $this->redirectToRoute('game', array('id' => $game->getId()));
+    $this->setAccess($response, $game);    
+    return $response;    
   }
 
   public function cancel($id) {
     $this->gameRepository->remove($id);
-    return $this->redirectToRoute('index');
+
+    $response = $this->redirectToRoute('index');
+    $this->removeAccess($response);
+    return $response;
   }
 
-  public function move(string $id, int  $tile) {
+  public function move(Request $request, string $id, int $tile) {
     $game = $this->gameRepository->findGameById($id);
-    if (!$game->getIsVictory()) {
+
+    if ($this->hasAccess($request, $game) && !$game->getIsVictory()) {
       $newGame = $this->api->move($game, $tile);
       $this->gameRepository->save($newGame);
     }
+
     return $this->redirectToRoute('game', array('id' => $id));    
   }
 }
