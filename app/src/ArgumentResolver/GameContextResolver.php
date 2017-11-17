82ae317b64a02d8a80bb076fedbcd6cfd44f38ed
Luwangel <adrien.amoros@gmail.com>
2017-11-17 17:16:09 +0100

    Change the context resolvers to accept the authorization's header

--- app/src/ArgumentResolver/GameContextResolver.php
+++ app/src/ArgumentResolver/GameContextResolver.php
@@ -7,7 +7,9 @@ use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use App\ArgumentResolver\GameContext;
 use App\Authentication\CookieAuthManager;
+use App\Authentication\TokenAuthManager;
 use App\Repository\GameRepository;
+
 
 class GameContextResolver implements ArgumentValueResolverInterface {
   private $gameRepository;
@@ -22,8 +24,9 @@ class GameContextResolver implements ArgumentValueResolverInterface {
 
   public function resolve(Request $request, ArgumentMetadata $argument) {
     $game = $this->gameRepository->findGameById($request->get('id'));
-    $isPlayer = CookieAuthManager::isPlayer($request, $game);
-    
+    $token = $request->headers->get('Authorization');
+
+    $isPlayer = $token ? TokenAuthManager::isPlayer($request, $game, $token) : CookieAuthManager::isPlayer($request, $game);
     $gameContext = new GameContext();
     $gameContext->setGame($game);
     $gameContext->setIsPlayer($isPlayer);
---
+++ app/src/Authentication/TokenAuthManager.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Authentication;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Game;
+use App\Entity\Player;
+
+class TokenAuthManager {
+  public static function isPlayer(Request $request, Game $game, string $token) : bool {
+    $player1 = $game->getPlayer1();
+    $player2 = $game->getPlayer2();
+    return $player1->getToken() == $token || !$game->isFull() && $player2->getToken() == $token;
+  }
+
+  public static function getPlayer(Request $request, Game $game, string $token) {
+    if ($game->getIsMultiplayer() && $game->getPlayer2() && $game->getPlayer2()->getToken() == $token) {
+      return $game->getPlayer2();
+    }
+    return $game->getPlayer1();
+  }
+}
